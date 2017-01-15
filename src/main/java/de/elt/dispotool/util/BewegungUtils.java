3281e64e0e2e1ceaa703faf495ec27b85bb010c4
Jonas <Jonaseicher@hotmail.com>
2017-01-15 20:07:06 +0100

    commit before aws deploy. reorderpoint hat nur dummy-page

---
+++ src/main/java/de/elt/dispotool/util/BewegungUtils.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.elt.dispotool.util;
+
+import de.elt.dispotool.entities.Bewegung;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import lombok.extern.java.Log;
+import org.apache.commons.lang3.time.DateUtils;
+
+/**
+ *
+ * @author Jonas
+ */
+@Log
+public class BewegungUtils {
+
+    public static int getVorzeichenOfType(String bwaTyp) {
+        switch (bwaTyp) {
+            case "NICHTS":
+                return 0;
+            case "ZUGANG":
+                return 1;
+            case "ABGANG":
+                return -1;
+            default:
+                log.log(Level.WARNING, "Default case in switch. Value was: {0}", bwaTyp);
+                return 0;
+        }
+    }
+    
+    
+    
+       public static SortedMap<String, SortedMap<String, Integer>> makeEmptyMap(Date first, Date last) {
+        
+        SortedMap<String, SortedMap<String, Integer>>  map = new TreeMap();
+        Date lastPlusOne = DateUtils.addDays(last, 1);
+        
+        SimpleDateFormat format = new SimpleDateFormat("yyyy,MM,dd");
+        for (Date day = first; day.before(lastPlusOne); day = DateUtils.addDays(day, 1)) {
+            String dateString = format.format(day);
+            map.put(dateString, new TreeMap<>());           
+        }
+        return map;
+    }
+    
+    public static void addSeries(SortedMap<String, SortedMap<String,Integer>> toMap, Map<String, Integer> series, String label) {
+        
+        for (Map.Entry<String, SortedMap<String,Integer>> entry : toMap.entrySet()) {
+            String dateString = entry.getKey();
+            if (series.get(dateString) == null) { continue; }
+            Map<String,Integer> subMap = entry.getValue();
+            subMap.put(label, series.get(dateString));
+        }
+    }
+    
+        // TODO: make combined method for initializing date fields first last
+    public static Date getFirstDate(List<Bewegung> bewegungen) {
+        Date first = new Date();
+        for (Bewegung b : bewegungen) {
+            Date x = b.getBuchungsdatum();
+            if (x.before(first)) {
+                first = x;
+            }
+        }
+        return first;
+    }
+
+    public static Date getLastDate(List<Bewegung> bewegungen) {
+        Date last = new Date(0l);
+        for (Bewegung b : bewegungen) {
+            Date x = b.getBuchungsdatum();
+            if (x.after(last)) {
+                last = x;
+            }
+        }
+        return last;
+    }
+}
---
+++ src/main/java/de/elt/dispotool/util/ChartUtils.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.elt.dispotool.util;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import lombok.extern.java.Log;
+
+/**
+ *
+ * @author Jonas
+ */
+@Log
+public class ChartUtils {
+    
+     /**
+     * Method that creates the chart data format that google charts wants:
+     *
+     * var dt = new google.visualization.DataTable({ cols: [{id: 'task', label:
+     * 'Task', type: 'string'}, {id: 'hours', label: 'Hours per Day', type:
+     * 'number'}], rows: [{c:[{v: 'Work'}, {v: 11}]}, {c:[{v: 'Eat'}, {v: 2}]},
+     * {c:[{v: 'Commute'}, {v: 2}]}, {c:[{v: 'Watch TV'}, {v:2}]}, {c:[{v:
+     * 'Sleep'}, {v:7, f:'7.000'}]}] });
+     *
+     * @return String to be passed to DataTable constructor.
+     * @param data A map with Labels, each pointing to a map with dates (in
+     * yyyy,MM,dd format) and values (Integers)
+     */
+    public static String makeChartData(SortedMap<String, SortedMap<String, Integer>> data) {
+
+        String cols = "{id:\"date\",label:\"Datum\",type:\"date\"}";
+
+        Set set = new HashSet();
+        for (SortedMap subMap : data.values()) {
+            set.addAll(subMap.keySet());
+        }
+        List<String> labels = new ArrayList(set);
+
+        for (String label : labels) {
+            cols += ",{id:\"" + label + "\",label:\"" + label + "\",type:\"number\"}";
+        }
+
+        String rows = "";
+        boolean first = true;
+        for (String dateKey : data.keySet()) {
+            String row = "";
+            if (!first) {
+                row += ",";
+            } else {
+                first = false;
+            }
+            row += "{c:[{v:\"Date(" + dateKey + ")\"}";
+            SortedMap<String, Integer> rowData = data.get(dateKey);
+
+            for (int i = 0; i < labels.size(); i++) {
+                String label = labels.get(i);
+                Integer value = rowData.get(label);
+                if (value == null) {
+                    value = 0;
+                }
+                row += ",{v:" + value + "}";
+            }
+            row += "]}";
+            rows += row;
+        }
+
+        String all = "{ cols: [" + cols + "], rows: [" + rows + "] }";
+
+        log.info(all);
+
+        return all;
+    }
+    
+}
