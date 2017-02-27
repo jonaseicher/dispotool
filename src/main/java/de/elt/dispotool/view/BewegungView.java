2a8179c75dcb664c526dd35c4e42df095fb6c012
Jonas <Jonaseicher@hotmail.com>
2017-02-27 10:45:49 +0100

    setup for local dev. workaround for failing delegate lombok annotation.

--- src/main/java/de/elt/dispotool/view/BewegungView.java
+++ src/main/java/de/elt/dispotool/view/BewegungView.java
@@ -5,9 +5,17 @@
  */
 package de.elt.dispotool.view;
 
+import de.elt.dispotool.dao.BewegungDao;
+import de.elt.dispotool.dao.BwaDao;
+import de.elt.dispotool.dao.MaterialDao;
+import de.elt.dispotool.entities.Bewegung;
+import de.elt.dispotool.entities.Bwa;
 import de.elt.dispotool.service.BewegungService;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.SortedMap;
 import javax.annotation.PostConstruct;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
@@ -17,6 +25,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Delegate;
 import lombok.extern.java.Log;
+import org.primefaces.model.chart.BarChartModel;
 
 /**
  *
@@ -28,7 +37,6 @@ import lombok.extern.java.Log;
 public class BewegungView implements Serializable {
 
     @Inject
-    @Delegate
     BewegungService bewegungService;
 
     @PostConstruct
@@ -45,4 +53,195 @@ public class BewegungView implements Serializable {
         bestellmenge = newValue;
         //bewegungService.setMaterialNummer("10010844");
     }
+
+    public void initBewegungsMap() {
+        bewegungService.initBewegungsMap();
+    }
+
+    public void initChartMap() {
+        bewegungService.initChartMap();
+    }
+
+    public SortedMap<String, Integer> getBestandsMap() {
+        return bewegungService.getBestandsMap();
+    }
+
+    public Integer getAnfangsbestand() {
+        return bewegungService.getAnfangsbestand();
+    }
+
+    public void initBestandsMap() {
+        bewegungService.initBestandsMap();
+    }
+
+    public String getBewegungsArray() {
+        return bewegungService.getBewegungsArray();
+    }
+
+    public String getChartArray() {
+        return bewegungService.getChartArray();
+    }
+
+    public String getBestandsArray() {
+        return bewegungService.getBestandsArray();
+    }
+
+    public List<Bwa> getBwasOfMaterial() {
+        return bewegungService.getBwasOfMaterial();
+    }
+
+    public List<Bwa> getBwasOfMaterial(String materialNummer) {
+        return bewegungService.getBwasOfMaterial(materialNummer);
+    }
+
+    public void setMaterialNummer(String matnr) {
+        bewegungService.setMaterialNummer(matnr);
+    }
+
+    public int getMaxAbgangsmenge() {
+        return bewegungService.getMaxAbgangsmenge();
+    }
+
+    public Integer getMengeOfBwa(String bwa) {
+        return bewegungService.getMengeOfBwa(bwa);
+    }
+
+    public BewegungDao getBewegungDao() {
+        return bewegungService.getBewegungDao();
+    }
+
+    public BwaDao getBwaDao() {
+        return bewegungService.getBwaDao();
+    }
+
+    public MaterialDao getMaterialDao() {
+        return bewegungService.getMaterialDao();
+    }
+
+    public List<Bewegung> getBewegungen() {
+        return bewegungService.getBewegungen();
+    }
+
+    public List<Bewegung> getZugaenge() {
+        return bewegungService.getZugaenge();
+    }
+
+    public List<Bewegung> getAbgaenge() {
+        return bewegungService.getAbgaenge();
+    }
+
+    public int getAbgangsInterval() {
+        return bewegungService.getAbgangsInterval();
+    }
+
+    public Date getFirst() {
+        return bewegungService.getFirst();
+    }
+
+    public Date getLast() {
+        return bewegungService.getLast();
+    }
+
+    public List<Bwa> getBwas() {
+        return bewegungService.getBwas();
+    }
+
+    public Map<String, Integer> getVorzeichen() {
+        return bewegungService.getVorzeichen();
+    }
+
+    public SortedMap<String, SortedMap<String, Integer>> getBewegungsMap() {
+        return bewegungService.getBewegungsMap();
+    }
+
+    public SortedMap<String, SortedMap<String, Integer>> getChartMap() {
+        return bewegungService.getChartMap();
+    }
+
+    public Map<String, Integer> getBwaMengen() {
+        return bewegungService.getBwaMengen();
+    }
+
+    public String getMaterialNummer() {
+        return bewegungService.getMaterialNummer();
+    }
+
+    public BarChartModel getBarChartModel() {
+        return bewegungService.getBarChartModel();
+    }
+
+    public List<String> getTypes() {
+        return bewegungService.getTypes();
+    }
+
+    public void setBewegungDao(BewegungDao bewegungDao) {
+        bewegungService.setBewegungDao(bewegungDao);
+    }
+
+    public void setBwaDao(BwaDao bwaDao) {
+        bewegungService.setBwaDao(bwaDao);
+    }
+
+    public void setMaterialDao(MaterialDao materialDao) {
+        bewegungService.setMaterialDao(materialDao);
+    }
+
+    public void setBewegungen(List<Bewegung> bewegungen) {
+        bewegungService.setBewegungen(bewegungen);
+    }
+
+    public void setZugaenge(List<Bewegung> zugaenge) {
+        bewegungService.setZugaenge(zugaenge);
+    }
+
+    public void setAbgaenge(List<Bewegung> abgaenge) {
+        bewegungService.setAbgaenge(abgaenge);
+    }
+
+    public void setAbgangsInterval(int abgangsInterval) {
+        bewegungService.setAbgangsInterval(abgangsInterval);
+    }
+
+    public void setFirst(Date first) {
+        bewegungService.setFirst(first);
+    }
+
+    public void setLast(Date last) {
+        bewegungService.setLast(last);
+    }
+
+    public void setBwas(List<Bwa> bwas) {
+        bewegungService.setBwas(bwas);
+    }
+
+    public void setVorzeichen(Map<String, Integer> vorzeichen) {
+        bewegungService.setVorzeichen(vorzeichen);
+    }
+
+    public void setBewegungsMap(SortedMap<String, SortedMap<String, Integer>> bewegungsMap) {
+        bewegungService.setBewegungsMap(bewegungsMap);
+    }
+
+    public void setBestandsMap(SortedMap<String, Integer> bestandsMap) {
+        bewegungService.setBestandsMap(bestandsMap);
+    }
+
+    public void setChartMap(SortedMap<String, SortedMap<String, Integer>> chartMap) {
+        bewegungService.setChartMap(chartMap);
+    }
+
+    public void setBwaMengen(Map<String, Integer> bwaMengen) {
+        bewegungService.setBwaMengen(bwaMengen);
+    }
+
+    public void setBarChartModel(BarChartModel barChartModel) {
+        bewegungService.setBarChartModel(barChartModel);
+    }
+
+    public void setTypes(List<String> types) {
+        bewegungService.setTypes(types);
+    }
+    
+    
+    
 }
